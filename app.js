d6a0dbe089a2a26b7c825a157c058570d5596732
ia-lerimund <93839547+ia-lerimund@users.noreply.github.com>
2022-09-19 12:36:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 let numOfLikesGogh = 492;
 let numOfLikesCourbet = 502;
 let numOfLikesDucreux = 137;
+let condition1 = true;
+let condition2 = true;
+let condition3 = true;
 const likesGogh = document.querySelector(".likesGogh");
 const likesCourbet = document.querySelector(".likesCourbet");
 const likesDucreux = document.querySelector(".likesDucreux");
@@ -9,14 +12,35 @@ const heartCourbet = document.querySelector(".heartCourbet");
 const heartDucreux = document.querySelector(".heartDucreux");
 
 heartGogh.addEventListener("click", function () {
-	numOfLikesGogh += 1;
-	likesGogh.innerHTML = `21 ${numOfLikesGogh} likes`;
+	if (condition1) {
+		numOfLikesGogh += 1;
+		condition1 = false;
+		likesGogh.innerHTML = `21 ${numOfLikesGogh} likes`;
+	} else {
+		numOfLikesGogh -= 1;
+		condition1 = true;
+		likesGogh.innerHTML = `21 ${numOfLikesGogh} likes`;
+	}
 });
 heartCourbet.addEventListener("click", function () {
-	numOfLikesCourbet += 1;
-	likesCourbet.innerHTML = `12 ${numOfLikesCourbet} likes`;
+	if (condition2) {
+		numOfLikesCourbet += 1;
+		condition2 = false;
+		likesCourbet.innerHTML = `12 ${numOfLikesCourbet} likes`;
+	} else {
+		numOfLikesCourbet -= 1;
+		condition2 = true;
+		likesCourbet.innerHTML = `12 ${numOfLikesCourbet} likes`;
+	}
 });
 heartDucreux.addEventListener("click", function () {
-	numOfLikesDucreux += 1;
-	likesDucreux.innerHTML = `15 ${numOfLikesDucreux} likes`;
+	if (condition3) {
+		numOfLikesDucreux += 1;
+		condition3 = false;
+		likesDucreux.innerHTML = `15 ${numOfLikesDucreux} likes`;
+	} else {
+		numOfLikesDucreux -= 1;
+		condition3 = true;
+		likesDucreux.innerHTML = `15 ${numOfLikesDucreux} likes`;
+	}
 });
